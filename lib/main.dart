f22f70a6fff54f9aad03ab70ea31243e2f28ab75
intenf from bigboss <taichi.nakamura2104@gmail.com>
2020-11-08 12:58:57 +0900

    終了

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int state = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: FlatButton(
+            onPressed: () {
+              print('I got clicked');
+              setState(() {
+                state = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$state.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
